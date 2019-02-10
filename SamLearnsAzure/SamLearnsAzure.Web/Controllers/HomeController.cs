7300505be5b81ba0634bebaf09f6da94d00f56e2
Sam Smith <samsmithnz@gmail.com>
2019-02-10 19:50:24 +0000

    Merged PR 242: Added website, YAML and ready to release
    Added website, YAML and ready to release
    
    Related work items: #788, #789

---
+++ SamLearnsAzure/SamLearnsAzure.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SamLearnsAzure.Web.Models;
+using System.Diagnostics;
+
+namespace SamLearnsAzure.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View(model: Configuration["AppSettings:Environment"]);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SamLearnsAzure/SamLearnsAzure.Web/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace SamLearnsAzure.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    //Load the appsettings.json configuration file
+                    IConfigurationRoot builtConfig = config.Build();
+                })
+                .UseStartup<Startup>();
+    }
+}
