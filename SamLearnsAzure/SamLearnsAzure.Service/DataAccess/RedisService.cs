833f0bad792ad51910545ab11d37e10150c0ce72
Sam Smith <samsmithnz@gmail.com>
2019-03-18 00:36:57 +0000

    Merged PR 271: Added Redis to the Arm template
    Added Redis to the Arm template
    
    Related work items: #855

---
+++ SamLearnsAzure/SamLearnsAzure.Service/DataAccess/RedisService.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using StackExchange.Redis;
+using System;
+using System.Diagnostics;
+
+namespace SamLearnsAzure.Service.DataAccess
+{
+
+    public class RedisService : IRedisService
+    {
+        private readonly IDatabase _database;
+
+        public RedisService(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            string result = null;
+            if (_database.IsConnected(key) == true)
+            {
+                result = await _database.StringGetAsync(key);
+                if (string.IsNullOrEmpty(result) == false)
+                {
+                    Debug.WriteLine("Getting item from cache: " + key);
+                }
+            }
+            return result;
+        }
+
+        public async Task<bool> SetAsync(string key, string data, TimeSpan expirationTime)
+        {
+            if (_database.IsConnected(key) == true)
+            {
+                Debug.WriteLine("Setting item into cache: " + key);
+                return await _database.StringSetAsync(key, data, expirationTime);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
