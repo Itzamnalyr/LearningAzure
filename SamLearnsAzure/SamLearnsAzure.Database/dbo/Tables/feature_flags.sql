2dfe3722eaac7fef10ef05a8a55d97dee0a46ba5
Sam Smith <samsmithnz@gmail.com>
2019-09-05 13:08:11 +0000

    Merged PR 357: Added database table
    Added database table
    
    Related work items: #1483

---
+++ SamLearnsAzure/SamLearnsAzure.Database/dbo/Tables/feature_flags.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[feature_flags]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[Name] VARCHAR(50),
+	[EnabledInDev] BIT,
+	[EnabledInQA] BIT,
+	[EnabledInProd] BIT,
+	[LastUpdated] DATETIME
+)
---
+++ SamLearnsAzure/SamLearnsAzure.Models/FeatureFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamLearnsAzure.Models
+{
+    public partial class FeatureFlags
+    {
+        int Id{ get; set; }
+        string Name{ get; set; }
+        bool EnabledInDev{ get; set; }
+        bool EnabledInQA{ get; set; }
+        bool EnabledInProd{ get; set; }
+        DateTime LastUpdated{ get; set; }
+    }
+}
