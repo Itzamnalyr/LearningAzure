8a25c3048bafb2e346b3216ce380c0a794846f19
Sam Smith <samsmithnz@gmail.com>
2019-11-15 15:58:45 +0000

    Merged PR 382: Updated security tasks
    Added security tasks
    
    Related work items: #1755

--- SamLearnsAzure/SamLearnsAzure.Service/Startup.cs
+++ SamLearnsAzure/SamLearnsAzure.Service/Startup.cs
@@ -83,7 +83,7 @@ namespace SamLearnsAzure.Service
             services.AddScoped<IPartImagesRepository, PartImagesRepository>();
 
             //Application insights initialization
-            services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
