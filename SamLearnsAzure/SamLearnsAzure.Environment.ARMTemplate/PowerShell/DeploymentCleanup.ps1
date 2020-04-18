78eb71a50c245586c9f18ab542d693f6f78e7a61
Sam Smith <samsmithnz@gmail.com>
2020-04-18 17:54:11 +0000

    Fix to address deployment quota of 800 deployments
    Fix to address deployment quota of 800 deployments

---
+++ SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/DeploymentCleanup.ps1
@@ -0,0 +1,33 @@
+﻿#######################################################################################
+#Clean up deployments older than 30 days to ensure we don't hit the limit of 800 deployments: 
+#######################################################################################
+param
+(
+	[string] $ResourceGroupName
+)
+
+Get-AzResourceGroup -Name $ResourceGroupName -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{
+    # ResourceGroup doesn't exist, do nothing
+    Write-Host "Resource group $ResourceGroupName does not (yet) exist"
+}
+else
+{
+    # ResourceGroup exist
+    Write-Host "Getting a list of all deployments older than 30 days"
+    $daysOlderToDelete = 30
+    $deployments = Get-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName | Where-Object Timestamp -lt ((Get-Date).AddDays(-$daysOlderToDelete))
+
+
+    Write-Host "removing deployments " $deployments.Length
+    $deploymentsDone = 0
+    foreach ($deployment in $deployments) {
+        Write-Host ($deployments.Length - $deploymentsDone) " deployments left!"
+        $deploymentsDone++
+        Remove-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name $deployment.DeploymentName
+    }
+
+    Write-Host "Deployment cleanup completed " $deployments.Length
+}
+
