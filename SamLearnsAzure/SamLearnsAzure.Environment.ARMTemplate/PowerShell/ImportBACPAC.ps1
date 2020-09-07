2a774cb378e810bfb97376bf1ca8991002fad3e5
Sam Smith <samsmithnz@gmail.com>
2020-09-07 10:58:47 -0400

    Updated database restore script

--- SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/ImportBACPAC.ps1
+++ SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/ImportBACPAC.ps1
@@ -46,48 +46,27 @@ if ($RunImportAlways -eq $false)
 #Run the import if the override $RunImportAlways is set to true, or no rows were found in the database
 if ($RunImportAlways -eq $true)
 {
-	# Check if the Storage Context exists. The storage container is required to backup the database
-	Write-Host "Checking storage for container blob $StorageContainerName"
-	$StorageContext = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-	$StorageContainer = Get-AzStorageContainer -Name $StorageContainerName -Context $StorageContext
-
-	# Create the storage if it doesn't exist
-	if (!$StorageContainer)
-	{
-		Write-Error "Container $StorageContainerName does not exist. Import aborted"
-	}
-
-	# Generate the credentials required to connect to the SQL database
-	Write-Host "Generating credentials" 
-	$SecurePassword = ConvertTo-SecureString -String $ServerPassword -AsPlainText -Force
-	$Creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServerAdmin, $SecurePassword
-
-	# Generate a connection to the file
-	$Blobs = Get-AzStorageBlob -Container $StorageContainerName -Context $StorageContext -Prefix $DatabaseName
-	$BacpacFilename = $Blobs | Sort-Object -Property @{Expression={$_.LastModified}} -Descending | Select-Object Name, LastModified -First 1
+	# Get the last file in the blob
+    $Blobs = az storage blob list --container-name $StorageContainerName --prefix $DatabaseName --account-name $StorageAccountName --account-key $StorageAccountKey
+	$Blobs = $Blobs | ConvertFrom-Json 
+    $BacpacFilename = $Blobs[-1].name #Get the last item in the array
 	if (!$BacpacFilename)
 	{
 		Write-Error "BACPAC does not exist in container $StorageContainerName. Import aborted"
 	}
-	Write-Host "Found file to import" $BacpacFilename.Name
-	$BacpacUri = $StorageUri + $StorageContainerName + "/" + $BacpacFilename.Name
+	Write-Host "Found file to import" $BacpacFilename
+	$BacpacUri = $StorageUri + $StorageContainerName + "/" + $BacpacFilename
 
 	# Remove the current database, if it exists
 	Write-Host "Checking if database already exists..."
-	$ExistingDatabases = Get-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $DBServerName 
-	$ExistingDatabase = $ExistingDatabases | Where-Object {$_.DatabaseName -eq $DatabaseName}
+    $ExistingDatabases = az sql db list --resource-group $ResourceGroupName --server $DBServerName 
+	$ExistingDatabases = $ExistingDatabases | ConvertFrom-Json
+	$ExistingDatabase = $ExistingDatabases | Select name | Where-Object {$_.name -eq $DatabaseName} 
 	if ($ExistingDatabase)
 	{
 		Write-Host "Removing existing database..."
-		Remove-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $DBServerName -DatabaseName $DatabaseName -Force
-		do
-		{
-			Write-Host "Deleting in progress..." (Get-Date).ToString("HH:mm:ss.ff")
-			Start-Sleep -s 1
-			$DeletingExistingDatabase = Get-AzSqlDatabase -ResourceGroupName $ResourceGroupName -ServerName $DBServerName | Where-Object {$_.DatabaseName -eq $DatabaseName} 
-		}
-		while ($DeletingExistingDatabase -ne $null)
-		Write-Host "Delete done! " (Get-Date).ToString("HH:mm:ss.ff")
+        az sql db delete --name $DatabaseName --resource-group $ResourceGroupName --server $DBServerName --yes #--no-wait
+        Write-Host "Delete done! " (Get-Date).ToString("HH:mm:ss.ff")
 	}
 	else
 	{
@@ -111,19 +90,7 @@ if ($RunImportAlways -eq $true)
             az sql db import --resource-group $ResourceGroupName --server $DBServerName --name $DatabaseName --admin-user $ServerAdmin --admin-password $ServerPassword --storage-key "$StorageAccountKey" --storage-key-type StorageAccessKey --storage-uri "$BacpacUri"  
 			
             Write-Host "Finishing database import..." + (Get-Date).ToString("HH:mm:ss.ff")
-            #$importRequest = New-AzSqlDatabaseImport -ResourceGroupName $ResourceGroupName -ServerName $DBServerName -DatabaseName $DatabaseName -StorageKeytype "StorageAccessKey" -StorageKey $StorageAccountKey -StorageUri $BacpacUri -AdministratorLogin $Creds.UserName -AdministratorLoginPassword $Creds.Password -Edition $Edition -ServiceObjectiveName $ServiceObjectiveName -DatabaseMaxSizeBytes 5000000 -ErrorAction SilentlyContinue
-			#$importRequest
-
-			# Monitor import status, showing progress every 1 second
-			#$importStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink 
-			#while ($importStatus.Status -eq "InProgress")
-			#{
-			#	Write-Host "Import in progress..." + (Get-Date).ToString("HH:mm:ss.ff")
-			#	Start-Sleep -s 1
-			#	$importStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
-			#}
-			#$importStatus
-			$DatabaseRestoreSuccessful = "true"
+            $DatabaseRestoreSuccessful = "true"
 		}
 		catch 
 		{
