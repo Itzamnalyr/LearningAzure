6a3483b0426d689050e88e37b03f4ab7915e75dc
Sam Smith <samsmithnz@gmail.com>
2020-09-07 20:59:19 -0400

    Added the secret keys to key vault, moving it from the key vault file

--- SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/KeyVault.ps1
+++ SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/KeyVault.ps1
@@ -1,4 +1,5 @@
-﻿#######################################################################################
+﻿#TODO: Delete this file
+#######################################################################################
 #Extract important information from arm template variables and store in key vault
 #######################################################################################
 param
@@ -19,31 +20,31 @@ param
 
 Write-Host "Setting key vault secrets"
 #Get the application insights instrumentation key from the ARM Template outputs
-$applicationInsightsInstrumentationKeyName = "ApplicationInsights--InstrumentationKey$Environment"
-$applicationInsightsInstrumentationKeySecretvalue = ConvertTo-SecureString "$ApplicationInsightsInstrumentationKey" -AsPlainText -Force
+#$applicationInsightsInstrumentationKeyName = "ApplicationInsights--InstrumentationKey$Environment"
+#$applicationInsightsInstrumentationKeySecretvalue = ConvertTo-SecureString "$ApplicationInsightsInstrumentationKey" -AsPlainText -Force
 #$applicationInsightsInstrumentationKeyExistingSecretValue = (Get-AzKeyVaultSecret -vaultName "$KeyVaultName" -name "$applicationInsightsInstrumentationKeyName").SecretValueText
-Write-Host "Setting value $ApplicationInsightsInstrumentationKey for $applicationInsightsInstrumentationKeyName to key vault"
-Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$applicationInsightsInstrumentationKeyName" -SecretValue $applicationInsightsInstrumentationKeySecretvalue
+#Write-Host "Setting value $ApplicationInsightsInstrumentationKey for $applicationInsightsInstrumentationKeyName to key vault"
+#Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$applicationInsightsInstrumentationKeyName" -SecretValue $applicationInsightsInstrumentationKeySecretvalue
 
 #Get the storage account key from the ARM Template outputs
-$storageAccountName = "StorageAccountKey$Environment"
-$storageAccountSecretvalue = ConvertTo-SecureString "$StorageAccountKey" -AsPlainText -Force
+#$storageAccountName = "StorageAccountKey$Environment"
+#$storageAccountSecretvalue = ConvertTo-SecureString "$StorageAccountKey" -AsPlainText -Force
 #$storageAccountExistingSecretValue = (Get-AzKeyVaultSecret -vaultName "$KeyVaultName" -name "$storageAccountName").SecretValueText
-Write-Host "Setting value for $storageAccountName to key vault"
-Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$storageAccountName" -SecretValue $storageAccountSecretvalue
+#Write-Host "Setting value for $storageAccountName to key vault"
+#Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$storageAccountName" -SecretValue $storageAccountSecretvalue
 
 #Get the sql server connection string from the ARM Template outputs
-$sqlConnectionStringName = "ConnectionStrings--SamsAppConnectionString$Environment"
-$sqlConnectionStringSecretvalue = ConvertTo-SecureString "Server=tcp:$DatabaseServerName.database.windows.net,1433;Initial Catalog=samsdb;Persist Security Info=False;User ID=$DatabaseLoginName;Password=$DatabaseLoginPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" -AsPlainText -Force
+#$sqlConnectionStringName = "ConnectionStrings--SamsAppConnectionString$Environment"
+#$sqlConnectionStringSecretvalue = ConvertTo-SecureString "Server=tcp:$DatabaseServerName.database.windows.net,1433;Initial Catalog=samsdb;Persist Security Info=False;User ID=$DatabaseLoginName;Password=$DatabaseLoginPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;" -AsPlainText -Force
 #$sqlConnectionStringExistingSecretValue = (Get-AzKeyVaultSecret -vaultName "$KeyVaultName" -name "$sqlConnectionStringName").SecretValueText
-Write-Host "Setting value for $sqlConnectionStringName to key vault"
-Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$sqlConnectionStringName" -SecretValue $sqlConnectionStringSecretvalue
+#Write-Host "Setting value for $sqlConnectionStringName to key vault"
+#Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$sqlConnectionStringName" -SecretValue $sqlConnectionStringSecretvalue
 
 #Get the redis connection string from the ARM Template outputs
-$redisCacheConnectionStringName = "AppSettings--RedisCacheConnectionString$Environment"
-$redisCacheConnectionStringSecretvalue = ConvertTo-SecureString "$RedisConnectionString" -AsPlainText -Force
+#$redisCacheConnectionStringName = "AppSettings--RedisCacheConnectionString$Environment"
+#$redisCacheConnectionStringSecretvalue = ConvertTo-SecureString "$RedisConnectionString" -AsPlainText -Force
 #$redisCacheConnectionStringExistingSecretValue = (Get-AzKeyVaultSecret -vaultName "$KeyVaultName" -name "$redisCacheConnectionStringName").SecretValueText
-Write-Host "Setting value for $redisCacheConnectionStringName to key vault"
-Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$redisCacheConnectionStringName" -SecretValue $redisCacheConnectionStringSecretvalue
+#Write-Host "Setting value for $redisCacheConnectionStringName to key vault"
+#Set-AzKeyVaultSecret -VaultName "$KeyVaultName" -Name "$redisCacheConnectionStringName" -SecretValue $redisCacheConnectionStringSecretvalue
 
 Write-Host "key vault updates complete"
