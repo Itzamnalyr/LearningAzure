d81037d9551f56deed618ee85fd3c398c7ba7d4d
Sam Smith <samsmit@microsoft.com>
2020-09-18 19:10:16 +0000

    Merged PR 467: Added custom domains to front door
    Added custom domains to front door
    
    Related work items: #2154

--- SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/DeployInfrastructureFrontDoor.ps1
+++ SamLearnsAzure/SamLearnsAzure.Environment.ARMTemplate/PowerShell/DeployInfrastructureFrontDoor.ps1
@@ -6,7 +6,8 @@
 	[string] $resourceGroupLocation,
 	[string] $resourceGroupLocationShort,
 	[string] $dataKeyVaultName,
-	[string] $templatesLocation
+	[string] $templatesLocation,
+	[string] $customDomain
 )
 
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
@@ -21,18 +22,44 @@ Write-Host "resourceGroupLocation: $resourceGroupLocation"
 Write-Host "resourceGroupLocationShort: $resourceGroupLocationShort"
 Write-Host "dataKeyVaultName: $dataKeyVaultName"
 Write-Host "templatesLocation: $templatesLocation"
-
+Write-Host "customDomain: $customDomain"
 
 #Variables
 $frontDoorName = "$appPrefix-$environment-$resourceGroupLocationShort-frontdoor"
 $webSiteName = "$appPrefix-$environment-$resourceGroupLocationShort-web"
-$serviceAPIName = "$appPrefix-$environment-$resourceGroupLocationShort-service"
 $frontDoorBackEndAddresses = "['$webSiteName.azurewebsites.net']"  #create an array of strings for each of the back end pool resources
 $timing = -join($timing, "2. Variables created: ", $stopwatch.Elapsed.TotalSeconds, "`n");
 Write-Host "2. Variables created: "$stopwatch.Elapsed.TotalSeconds
 
-#Frontdoor
-az deployment group create --resource-group $resourceGroupName --name $frontDoorName --template-file "$templatesLocation\FrontDoor.json" --parameters frontDoorName=$frontDoorName frontDoorBackEndAddresses=$frontDoorBackEndAddresses 
+
+#Frontdoor ARM template
+az deployment group create --resource-group $resourceGroupName --name $frontDoorName --template-file "$templatesLocation\FrontDoor.json" --parameters frontDoorName=$frontDoorName frontDoorBackEndAddresses=$frontDoorBackEndAddresses
+
+#Add the extension first:
+az extension add --name front-door
+#Add Frontdoor custom domain to frontend-endpoint
+$FrontDoorFrontEndEndPointsJson = az network front-door frontend-endpoint list --front-door-name $frontDoorName --resource-group $resourceGroupName
+$FrontDoorFrontEndEndPoints = $FrontDoorFrontEndEndPointsJson | ConvertFrom-Json
+$FoundFrontEndPoint = $false
+#We can't create the frontend point if it already exists, so check again
+foreach($FrontDoorFrontEndEndPoint in $FrontDoorFrontEndEndPoints) {
+    if ($FrontDoorFrontEndEndPoint.name -eq $customDomain.Replace(".","-"))
+    {
+        $FoundFrontEndPoint = $true
+    }
+}
+if ($FoundFrontEndPoint -eq $false)
+{
+    az network front-door frontend-endpoint create --front-door-name $frontDoorName --host-name $customDomain --name $customDomain.Replace(".","-") --resource-group $resourceGroupName --session-affinity-enabled Disabled --session-affinity-ttl 0
+}
+
+#Add custom domain to routing rules
+az network front-door routing-rule update --front-door-name $frontDoorName --name "$frontDoorName-routing" --resource-group $resourceGroupName --frontend-endpoints "$frontDoorName-azurefd-net".Replace(".","-") $customDomain.Replace(".","-")
+#Debugging stuff for the routing
+#$routingRulesJson = az network front-door routing-rule list --front-door-name $frontDoorName --resource-group $resourceGroupName
+#$routingRules = $routingRulesJson  | ConvertFrom-Json 
+#$routingRules[0].frontendEndpoints
+
 $timing = -join($timing, "14. Frontdoor created: ", $stopwatch.Elapsed.TotalSeconds, "`n");
 Write-Host "14. Frontdoor created: "$stopwatch.Elapsed.TotalSeconds
 
